eb1737926d267259efe0e443d6cc4f3e45aadbac
Brent Cleary <brent.cleary@gmail.com>
2023-12-07 19:32:52 -0800

    DataBase Retrieval Working

--- BlazorMovieLive/Models/PopularMoviePagedResponse.cs
+++ BlazorMovieLive/Models/PopularMoviePagedResponse.cs
@@ -1,10 +1,19 @@
-﻿namespace BlazorMovieLive.Models
+﻿using System.Text.Json.Serialization;
+
+namespace BlazorMovieLive.Models
 {
 	public class PopularMoviePagedResponse
 	{
-		public int page { get; set; }
-		public PopularMovie[] results { get; set; }
-		public int total_pages { get; set; }
-		public int total_results { get; set; }
+		[JsonPropertyName("page")]
+		public int Page { get; set; }
+
+		[JsonPropertyName("results")]
+		public IEnumerable<PopularMovie> Results { get; set; } = [];
+		
+		[JsonPropertyName("total_pages")]
+		public int TotalPages { get; set; }
+		
+		[JsonPropertyName("total_results")]
+		public int TotalResults { get; set; }
 	}
 }
